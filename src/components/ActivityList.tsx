34d16e9e2a027c2d150444e2edf50948d745d2d8
Carlos-could <jjbbpe@gmail.com>
2025-06-07 07:12:54 +0200

    150

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,4 +1,6 @@
 import { Activity } from "../types"
+import { categories } from "../data/categories"
+import { useMemo } from "react"
 
 type ActivityListProps = {
   activities: Activity[]
@@ -6,7 +8,7 @@ type ActivityListProps = {
 
 export default function ActivityList({ activities }: ActivityListProps) {
 
-  console.log(activities)
+  const categoryName = useMemo(()=> (category: Activity['category']) => categories.map(cat => cat.id === category ? cat.name : ''), [activities])
   return (
     <>
       <h2 className='text-4xl text-slate-600 text-center'>
@@ -15,8 +17,8 @@ export default function ActivityList({ activities }: ActivityListProps) {
       {activities.map(activity => (
         <div key={activity.id} className="px-5 py-10 bg-white mt-5 flex justify-between">
           <div className="space-y-2 relative">
-            <p>
-              {activity.category}
+            <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-medium ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
+              {categoryName(activity.category)}
             </p>
             <p className="text-2xl pt-5">{activity.name}</p>
             <p className="text-4xl text-lime-500">{activity.calories} {''}
